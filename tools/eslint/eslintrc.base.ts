207b48733e9c7d07efdc57ac9a7a5dd94ae10192
silo9506 <ghrn97@gmail.com>
2025-05-27 12:40:40 +0900

    style(tools) : eslint 기본설정파일 세미콜론 추가 #12

--- tools/eslint/eslintrc.base.ts
+++ tools/eslint/eslintrc.base.ts
@@ -1,33 +1,39 @@
-import type { Linter } from 'eslint'
+import type { Linter } from "eslint";
 
 export const baseConfig: Linter.Config = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'jsx-a11y', 'tailwindcss'],
+  parser: "@typescript-eslint/parser",
+  plugins: [
+    "@typescript-eslint",
+    "react",
+    "react-hooks",
+    "jsx-a11y",
+    "tailwindcss",
+  ],
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:tailwindcss/recommended',
-    'prettier'
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:tailwindcss/recommended",
+    "prettier",
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
     ecmaFeatures: {
-      jsx: true
-    }
+      jsx: true,
+    },
   },
   settings: {
     react: {
-      version: 'detect'
-    }
+      version: "detect",
+    },
   },
   rules: {
     // 필요에 따라 rule 추가
-    'react/react-in-jsx-scope': 'off', // React 17+
-    'tailwindcss/no-custom-classname': 'off'
-  }
-}
+    "react/react-in-jsx-scope": "off", // React 17+
+    "tailwindcss/no-custom-classname": "off",
+  },
+};
