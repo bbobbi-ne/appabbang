d72f34a2b6c9053f6249860e3cc13665151518e3
silo9506 <ghrn97@gmail.com>
2025-05-28 21:34:14 +0900

    feat(ui) : 추가한 ui 컴포넌트 export #21

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,4 +1,31 @@
 import '@appabbang/tailwind/style.css';
 
-export { Button } from './src/components/atom/button';
-export type { Props as ButtonProps } from './src//components/atom/button';
+export { Button } from './src/components/ui/button';
+export type { ButtonProps } from './src//components/ui/button';
+export { Avatar, AvatarFallback, AvatarImage } from './src/components/ui/avatar';
+export {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './src/components/ui/card';
+export {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  useFormField,
+} from './src//components/ui/form';
+export { Input, PasswordInput } from './src/components/ui/input';
+export { Label } from './src//components/ui/label';
+export {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from './src//components/ui/tooltip';
