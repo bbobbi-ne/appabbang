09a3932e0167b0d303a753c3983a64bf9229df6e
jieun-c <flffl1231@naver.com>
2025-05-29 10:46:47 +0900

    test(admin): 내정보조회 샘플 api 연동 #23

--- apps/admin/src/routes/index.tsx
+++ apps/admin/src/routes/index.tsx
@@ -1,11 +1,24 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { Button } from '@appabbang/ui'
-
+import { createFileRoute } from '@tanstack/react-router';
+import { Button } from '@appabbang/ui';
+import axios from 'axios';
+import { useCallback, useMemo } from 'react';
+import { useEffect } from 'react';
 export const Route = createFileRoute('/')({
   component: App,
-})
+});
 
 function App() {
+  const getMe = useCallback(async () => {
+    const response = await axios.get(`${import.meta.env.VITE_APPABBANG_API_URL}/auth/me`);
+    console.log(response);
+    console.log(response.data);
+    return response.data;
+  }, []);
+
+  useEffect(() => {
+    getMe();
+  }, []);
+
   return (
     <div className="p-4">
       <div className="text-3xl">프로젝트 시작중..</div>
@@ -13,5 +26,5 @@ function App() {
 
       <Button>Click me</Button>
     </div>
-  )
+  );
 }
