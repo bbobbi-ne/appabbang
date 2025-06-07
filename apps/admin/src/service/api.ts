46a2ad94b6db65db8d8067c99eb71ff1b9d42ba9
silo9506 <ghrn97@gmail.com>
2025-06-07 16:47:25 +0900

    feat(admin) : axios instance 및 interceptors추가 서버api 호출 함수 추가 #42

---
+++ apps/admin/src/service/api.ts
@@ -0,0 +1,56 @@
+import { baseInstance, requireAccessTokenInstance } from './instance';
+
+interface LoginRequest {
+  id: string;
+  pw: string;
+}
+
+interface LoginSuccessResponse {
+  success: true;
+  accessToken: string;
+}
+
+interface LoginErrorResponse {
+  success: false;
+  message: string;
+}
+
+export async function login(req: LoginRequest): Promise<LoginSuccessResponse | LoginErrorResponse> {
+  try {
+    const response = await baseInstance.post('/auth/login', req);
+
+    return {
+      success: true,
+      accessToken: response.data.accessToken,
+    };
+  } catch (error: any) {
+    const message = error.response?.data?.message || '로그인에 실패했습니다.';
+
+    return {
+      success: false,
+      message,
+    };
+  }
+}
+
+export async function getMe(req: string) {
+  try {
+    const res = await requireAccessTokenInstance.get('/auth/me', {
+      headers: { Authorization: `Bearer ${req}` },
+    });
+
+    return res.data;
+  } catch (error: any) {
+    return error;
+  }
+}
+
+export async function refresh() {
+  const response = await baseInstance.post('/auth/refresh');
+
+  try {
+    return response.data.accessToken;
+  } catch (error: any) {
+    return error.message;
+  }
+}
