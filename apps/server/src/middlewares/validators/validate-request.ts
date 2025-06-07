3aa3eb192ef09ea1991f910d3a34159e57f92862
jieun-c <flffl1231@naver.com>
2025-06-07 16:08:47 +0900

    feat(server): 로그인 유효성 검증 로직 추가

--- apps/server/src/middlewares/validators/validate-request.ts
+++ apps/server/src/middlewares/validators/validate-request.ts
@@ -1,5 +1,5 @@
 import { RequestHandler } from 'express';
-import { validationResult } from 'express-validator';
+import { validationResult, body } from 'express-validator';
 
 export const validate = (validators: RequestHandler[]): RequestHandler => {
   return (req, res, next) => {
@@ -20,3 +20,8 @@ export const validate = (validators: RequestHandler[]): RequestHandler => {
     })().catch(next);
   };
 };
+
+export const loginValidator = [
+  body('id').trim().notEmpty().withMessage('id 는 필수입니다'),
+  body('pw').trim().notEmpty().withMessage('pw 는 필수입니다'),
+];
