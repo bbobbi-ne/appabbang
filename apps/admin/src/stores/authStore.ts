07201202b1bdbaa4c8d4bf46dd9eaaf89ff39a0b
silo9506 <ghrn97@gmail.com>
2025-06-07 16:48:03 +0900

    feat(admin) : 유저정보 및 AccessToken을  담을 전역 상태관리 store추가 #42

---
+++ apps/admin/src/stores/authStore.ts
@@ -0,0 +1,29 @@
+import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
+
+interface AuthState {
+  accessToken: string | null;
+  setAccessToken: (token: string) => void;
+  clearAccessToken: () => void;
+  auth: any;
+  setAuth: (data: any) => void;
+  clearAuth: () => void;
+}
+
+export const useAuthStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      accessToken: null,
+      setAccessToken: (token) => set({ accessToken: token }),
+      clearAccessToken: () => set({ accessToken: null }),
+      auth: null,
+      setAuth: (data) => set({ auth: data }),
+      clearAuth: () => set({ auth: null }),
+    }),
+    {
+      name: 'auth-storage',
+      storage: createJSONStorage(() => sessionStorage),
+      partialize: (state) => ({ auth: state.auth }), // auth만 저장
+    },
+  ),
+);
