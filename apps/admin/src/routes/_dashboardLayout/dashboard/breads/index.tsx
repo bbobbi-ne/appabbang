153daad424bef32118ff702bc90bb4b84f6defef
silo9506 <ghrn97@gmail.com>
2025-06-22 18:47:16 +0900

    fix(admin): table column요소 다시 하나의 파일로 합, 함수 최상단에 BreadStatus값을 query요소로 불러와야하기 때문 파일 분리시 useQuery훅 사용 불가능 #51

--- apps/admin/src/routes/_dashboardLayout/dashboard/breads/index.tsx
+++ apps/admin/src/routes/_dashboardLayout/dashboard/breads/index.tsx
@@ -1,32 +1,31 @@
 import { createFileRoute } from '@tanstack/react-router';
 import * as React from 'react';
 import {
+  createColumnHelper,
   flexRender,
   getCoreRowModel,
   getFilteredRowModel,
   getPaginationRowModel,
   getSortedRowModel,
   useReactTable,
+  type ColumnDef,
   type ColumnFiltersState,
   type SortingState,
 } from '@tanstack/react-table';
 
 import {
+  AspectRatio,
   Button,
   Card,
   CardContent,
   CardFooter,
   CardHeader,
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-  Input,
-  Label,
+  Checkbox,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
   Table,
   TableBody,
   TableCell,
@@ -35,23 +34,211 @@ import {
   TableRow,
 } from '@appabbang/ui';
 
-import { columns, makeBreadsData } from '@/data/demo-brads';
 import { TablePagination } from '@/components/table-pagination';
 import { BreadsDialog } from '../../../../components/breads_dialog';
+import { useQuery } from '@tanstack/react-query';
+import { getBreads } from '@/service/breadApi';
+import { ArrowUpDown } from 'lucide-react';
+import { useBreadStatus } from '@/hooks/useBreadStatus';
+
+export type Breads = {
+  no: number;
+  name: string;
+  description: string;
+  unitPrice: number;
+  breadStatus: any;
+  image_url: string;
+  createdAt: Date;
+  updatedAt: Date;
+  image: string[] | string;
+};
 
 export const Route = createFileRoute('/_dashboardLayout/dashboard/breads/')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const [data] = React.useState(() => makeBreadsData(400));
-
   const [sorting, setSorting] = React.useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([]);
   const [pagination, setPagination] = React.useState({ pageIndex: 0, pageSize: 10 });
+  const { data } = useBreadStatus();
+  const breadStatus = data?.breadStatus;
 
-  const table = useReactTable({
-    data,
+  const columnHelper = createColumnHelper<Breads>();
+
+  const columns: ColumnDef<Breads, any>[] = [
+    columnHelper.display({
+      id: 'select',
+      header: ({ table }) => (
+        <Checkbox
+          className="mr-2"
+          checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && 'indeterminate')
+          }
+          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        />
+      ),
+      cell: ({ row }) => (
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+        />
+      ),
+    }),
+
+    columnHelper.accessor('no', {
+      header: ({ column }) => (
+        <Button
+          className="p-0"
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          번호 <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      ),
+      cell: (info) => {
+        return info.getValue();
+      },
+    }),
+
+    columnHelper.accessor('name', {
+      header: ({ column }) => (
+        <Button
+          className="p-0"
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          메뉴명 <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      ),
+      cell: ({ row }) => row.getValue('name'),
+    }),
+
+    columnHelper.accessor('description', {
+      header: '설명',
+      cell: (info) => (
+        <p className="line-clamp-3 whitespace-normal break-words">{info.getValue()}</p>
+      ),
+    }),
+
+    columnHelper.accessor('unitPrice', {
+      header: ({ column }) => (
+        <Button
+          className="p-0"
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          단가 <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      ),
+      cell: (info) =>
+        new Intl.NumberFormat('ko-KR', { style: 'currency', currency: 'KRW' }).format(
+          info.getValue(),
+        ),
+    }),
+
+    columnHelper.accessor('image', {
+      header: '대표이미지',
+      cell: ({ row }) => (
+        <AspectRatio>
+          <img
+            src={row.getValue('image')}
+            alt={row.original.name}
+            className="h-full w-full rounded-lg object-cover"
+          />
+        </AspectRatio>
+      ),
+    }),
+
+    columnHelper.accessor('breadStatus', {
+      header: ({ column }) => {
+        const rawValue = column.getFilterValue();
+        const value = typeof rawValue === 'string' ? rawValue : 'all';
+
+        return (
+          <Select
+            value={value}
+            onValueChange={(val) => {
+              column.setFilterValue(val === 'all' ? undefined : val);
+            }}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="상태" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="all">전체</SelectItem>
+              {breadStatus?.map(({ name, code }) => (
+                <SelectItem key={code} value={code}>
+                  {name}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        );
+      },
+
+      cell: (info) => {
+        const value = info.getValue();
+        const found = breadStatus?.find((item) => item.name === value);
+        return found?.name;
+      },
+
+      filterFn: (row, columnId, filterValue) => {
+        const columnValue = breadStatus?.find((item) => item.name === row.getValue(columnId));
+
+        return columnValue?.code === filterValue;
+      },
+
+      enableSorting: true,
+    }),
+
+    columnHelper.accessor('createdAt', {
+      header: ({ column }) => (
+        <Button
+          className="p-0"
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          생성일 <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      ),
+      cell: (info) =>
+        new Intl.DateTimeFormat('ko-KR', {
+          dateStyle: 'medium',
+          timeStyle: 'short',
+        }).format(new Date(info.getValue())),
+    }),
+
+    columnHelper.accessor('updatedAt', {
+      header: ({ column }) => (
+        <Button
+          className="p-0"
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          최근 수정일 <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      ),
+      cell: (info) =>
+        new Intl.DateTimeFormat('ko-KR', {
+          dateStyle: 'medium',
+          timeStyle: 'short',
+        }).format(new Date(info.getValue())),
+    }),
+  ];
+
+  const {
+    data: bradsData,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ['breads'],
+    queryFn: getBreads,
+  });
+
+  const table = useReactTable<Breads>({
+    data: bradsData?.breads,
     columns,
     state: {
       pagination,
@@ -66,6 +253,9 @@ function RouteComponent() {
     getFilteredRowModel: getFilteredRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
   });
+
+  if (isLoading) return <>로딩중</>;
+  if (isError) return <>에러</>;
 
   return (
     <>
