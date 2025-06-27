ae006bc5bf36bf6c0d1af86c74a5245788023f22
silo9506 <ghrn97@gmail.com>
2025-06-27 15:13:36 +0900

    feat(admin): 전역로딩 추가 , 패칭 및 뮤테이션 상태일시 자동로딩되도록 로직추가 #51

--- apps/admin/src/routes/__root.tsx
+++ apps/admin/src/routes/__root.tsx
@@ -4,16 +4,22 @@ import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import TanStackQueryLayout from '../integrations/tanstack-query/layout.tsx';
 
 import type { QueryClient } from '@tanstack/react-query';
+import { Toaster } from 'sonner';
+import { GlobalLoading } from '@/components/loading.tsx';
 export interface MyRouterContext {
   queryClient: QueryClient;
 }
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
-  component: () => (
-    <>
-      <Outlet />
-      <TanStackRouterDevtools />
-      <TanStackQueryLayout />
-    </>
-  ),
+  component: () => {
+    return (
+      <>
+        <GlobalLoading />
+        <Toaster richColors />
+        <Outlet />
+        <TanStackRouterDevtools />
+        <TanStackQueryLayout />
+      </>
+    );
+  },
 });
