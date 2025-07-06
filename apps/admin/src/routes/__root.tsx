7a566ef964286a5d40ead3d698ef1b77c1499ea2
silo9506 <ghrn97@gmail.com>
2025-07-06 17:10:32 +0900

    feat(admin) : sooner(Toaster) 요소에 테마전달 하도록 수정 #63

--- apps/admin/src/routes/__root.tsx
+++ apps/admin/src/routes/__root.tsx
@@ -1,21 +1,24 @@
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
-import TanStackQueryLayout from '../integrations/tanstack-query/layout.tsx';
+import TanStackQueryLayout from '@/integrations/tanstack-query/layout.tsx';
 
 import type { QueryClient } from '@tanstack/react-query';
-import { Toaster } from 'sonner';
+import { Toaster } from '@appabbang/ui';
 import { GlobalLoading } from '@/components/loading.tsx';
+import { useThemeStore } from '@/stores/themeStore';
 export interface MyRouterContext {
   queryClient: QueryClient;
 }
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => {
+    const theme = useThemeStore((s) => s.theme);
+
     return (
       <>
         <GlobalLoading />
-        <Toaster richColors />
+        <Toaster richColors theme={theme} />
         <Outlet />
         <TanStackRouterDevtools />
         <TanStackQueryLayout />
