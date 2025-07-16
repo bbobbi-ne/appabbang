cf484c56de77aadaac0e08e8e970f173696e88a8
jieun-c <flffl1231@naver.com>
2025-07-16 14:27:01 +0900

    feat(server): payment - 리스트 조회 API, 단건 조회API, 입금여부수정 API, 환불여부수정 API (입금여부와 환불여부 수정시 order 의 orderStatus 는 각 접수확인과 취소완료로 함께 변경됨) #93

---
+++ apps/server/src/controllers/payment.controller.ts
@@ -0,0 +1,56 @@
+import { Request, Response } from 'express';
+import * as paymentService from '@/services/payment.service';
+import * as orderService from '@/services/order.service';
+import { AppError } from '@/types';
+
+/** 결제 목록 조회 */
+export const getList = async (req: Request, res: Response) => {
+  const payments = await paymentService.getList();
+  res.status(200).json(payments);
+};
+
+/** 결제 상세 조회 */
+export const getOneByNo = async (req: Request, res: Response) => {
+  const { no } = req.params;
+  const payment = await paymentService.getOneByNo(Number(no));
+
+  if (!payment) {
+    throw AppError.notFound('해당하는 결제 내역이 없습니다.');
+  }
+
+  res.status(200).json(payment);
+};
+
+/** 결제 입금 확인 변경 */
+export const updatePaid = async (req: Request, res: Response) => {
+  const { no } = req.params;
+  const { isPaid, orderNo } = req.body;
+
+  await paymentService.update(Number(no), {
+    isPaid,
+    ...(isPaid && {
+      paidConfirmedAt: new Date(),
+    }),
+  });
+
+  // 11: 접수 완료
+  await orderService.updateOrderStatus(Number(orderNo), '11');
+  res.status(200).json({ message: '결제 입금 확인 완료' });
+};
+
+/** 결제 환불 확인 변경 */
+export const updateRefund = async (req: Request, res: Response) => {
+  const { no } = req.params;
+  const { isRefunded, orderNo } = req.body;
+
+  await paymentService.update(Number(no), {
+    isRefunded,
+    ...(isRefunded && {
+      refundConfirmedAt: new Date(),
+    }),
+  });
+
+  // 51: 취소 완료
+  await orderService.updateOrderStatus(Number(orderNo), '51');
+  res.status(200).json({ message: '결제 환불 취소 완료' });
+};
