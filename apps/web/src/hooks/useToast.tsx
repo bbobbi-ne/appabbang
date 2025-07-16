dcbb8af8d5866b3aa59c17e547a4deab58562dbe
MARUNGs <mi1764@naver.com>
2025-07-16 21:48:40 +0900

    feat(web): useToast() Custom Hook 개발

---
+++ apps/web/src/hooks/useToast.tsx
@@ -0,0 +1,34 @@
+/**
+ * [Toast 전용 hook]
+ * type을 기준으로 어떤 경우의 alert기능을 수행할 것인지 switch로 나누었다.
+ */
+
+import { toast } from 'sonner';
+
+interface ToastProps {
+  message: string;
+  type?: 'success' | 'error' | 'warning' | 'info';
+}
+
+function useToast() {
+  const addToast = ({ message, type = 'success' }: ToastProps) => {
+    switch (type) {
+      case 'success':
+        toast.success(message);
+        break;
+      case 'warning':
+        toast.warning(message);
+        break;
+      case 'info':
+        toast.info(message);
+        break;
+      default:
+        toast.error(message);
+        break;
+    }
+  };
+
+  return { addToast };
+}
+
+export default useToast;
--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -2,6 +2,7 @@ import { Outlet, createRootRouteWithContext } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
 import Header from '../components/Header';
+import { Toaster } from '@appabbang/ui';
 import TanStackQueryLayout from '../integrations/tanstack-query/layout.tsx';
 import type { QueryClient } from '@tanstack/react-query';
 
@@ -13,6 +14,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => (
     <>
       <Header />
+      <Toaster richColors position="top-center" duration={3000} />
       <Outlet />
       <TanStackRouterDevtools />
       <TanStackQueryLayout />
