be437caab57d4f6a2d73d89b85a3dfb398872077
jieun-c <flffl1231@naver.com>
2025-07-30 17:15:10 +0900

    feat(web): 배송지 목록 조회 (더미데이터) #139

--- apps/web/src/components/pages/address-page.tsx
+++ apps/web/src/components/pages/address-page.tsx
@@ -1,3 +1,62 @@
+import { Badge, Button } from '@appabbang/ui';
+
 export default function AddressPage() {
-  return <div>AddressPage</div>;
+  const data = testData;
+
+  return (
+    <>
+      <div className="flex justify-end mb-2">
+        <Button>배송지 추가</Button>
+      </div>
+
+      {data.map((item) => (
+        <div
+          key={item.no}
+          className="border-b p-2 cursor-pointer flex flex-col gap-2 hover:bg-muted"
+        >
+          <div className="flex items-center gap-4">
+            <p className="font-bold">{item.recipientName}</p>
+
+            {item.isDefault && <Badge variant="secondary">기본배송지</Badge>}
+          </div>
+          <p>
+            {item.address},&nbsp;{item.addressDetail}({item.zipCode})
+          </p>
+          <p className="text-sm">{item.recipientMobile}</p>
+        </div>
+      ))}
+
+      <div className="pb-20" />
+    </>
+  );
 }
+
+const testData = [
+  {
+    no: 1,
+    address: '서울특별시 강남구 역삼동 123-123',
+    addressDetail: '101동 101호',
+    zipCode: '12345',
+    recipientName: '김가나',
+    recipientMobile: '010-3020-1010',
+    isDefault: true,
+  },
+  {
+    no: 2,
+    address: '서울특별시 강남구 역삼동 123-123',
+    addressDetail: '101동 101호',
+    zipCode: '12345',
+    recipientName: '김가나',
+    recipientMobile: '010-3020-1010',
+    isDefault: false,
+  },
+  {
+    no: 3,
+    address: '서울특별시 강남구 역삼동 123-123',
+    addressDetail: '101동 101호',
+    zipCode: '12345',
+    recipientName: '김가나',
+    recipientMobile: '010-3020-1010',
+    isDefault: false,
+  },
+];
