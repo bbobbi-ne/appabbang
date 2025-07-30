748c9321a7166394313d350a0b2c82b76df839c3
MARUNGs <mi1764@naver.com>
2025-07-30 16:42:39 +0900

    feat(web): footer 컴포넌트 제거

--- apps/web/src/components/common/footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <div className="bg-gray-300 h-16 flex justify-center items-center text-center">
-      Copyright 2025. Appabbang Co. All rights reserved
-    </div>
-  );
-}
