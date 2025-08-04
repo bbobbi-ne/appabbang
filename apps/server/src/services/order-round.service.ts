639e0ff88d061bf8cce1bbefdc088508da3602fe
MARUNGs <mi1764@naver.com>
2025-08-04 19:08:44 +0900

    feat(web): 주문차수 API 트랜잭션 설정

--- apps/server/src/services/order-round.service.ts
+++ apps/server/src/services/order-round.service.ts
@@ -2,6 +2,7 @@ import { prisma } from '@/lib/prisma';
 import { UploadedFile } from 'express-fileupload';
 import * as ImageService from './image.service';
 import { AppError } from '@/types';
+import { PrismaClient, Prisma } from '@prisma/client';
 
 // 1. 주문차수 이미지 공통코드 조회 - 전역 변수로 저장
 let IMAGE_TARGET_TYPE_CODE: string | null = null;
@@ -9,7 +10,6 @@ let IMAGE_TARGET_TYPE_CODE: string | null = null;
 /** 주문차수 prop interface */
 interface IOrderRound {
   no: number;
-  seq: number;
   name: string;
   public_id?: string;
   breadNoList: number[];
@@ -49,7 +49,6 @@ export const getOrderRoundList = async () => {
       take: 10000,
       select: {
         no: true,
-        seq: true,
         name: true,
         startedAt: true,
         endedAt: true,
@@ -101,7 +100,6 @@ export const getOrderRound = async (no: number) => {
       where: { no },
       select: {
         no: true,
-        seq: true,
         name: true,
         startedAt: true,
         endedAt: true,
@@ -140,39 +138,42 @@ export const getOrderRound = async (no: number) => {
 };
 
 /**
- *  주문차수 등록(이미지 없음)
+ *  주문차수 등록(이미지 X)
  *  or: orderRound
  *  orb: orderRoundBread
  */
 export const createWithoutImage = async (
-  body: Pick<IOrderRound, 'seq' | 'name' | 'breadNoList' | 'startedAt' | 'endedAt'>,
+  body: Pick<IOrderRound, 'name' | 'breadNoList' | 'startedAt' | 'endedAt'>,
 ) => {
   try {
-    // 1. 주문차수 등록 :: orderRound
-    const orResult = await prisma.orderRound.create({
-      data: {
-        name: body.name,
-        startedAt: body.startedAt,
-        endedAt: body.endedAt,
-      },
+    const result = await prisma.$transaction(async (tx) => {
+      // 1. 주문차수 등록 :: orderRound
+      const orResult = await prisma.orderRound.create({
+        data: {
+          name: body.name,
+          startedAt: body.startedAt,
+          endedAt: body.endedAt,
+        },
+      });
+
+      // 2. 주문차수에 맞는 빵 목록 등록 :: orderRoundBread
+      const breadNoList = await Promise.all(
+        body.breadNoList.map(async (breadNo) => {
+          const { breadNo: resultBreadNo } = await createOrderRoundBread(tx, {
+            no: orResult.no,
+            breadNo,
+          });
+
+          return resultBreadNo;
+        }),
+      );
+
+      // 3. return model 생성 :: 주문차수 + (주문차수 + 빵) 목록
+      return { ...orResult, breadNoList, image: [] };
     });
 
-    // 2. 주문차수에 맞는 빵 목록 등록 :: orderRoundBread
-    const breadNoList = await Promise.all(
-      body.breadNoList.map(async (breadNo) => {
-        const { breadNo: resultBreadNo } = await createOrderRoundBread({
-          seq: orResult.seq,
-          breadNo,
-        });
-
-        return resultBreadNo;
-      }),
-    );
-
-    // 3. return model 생성 :: 주문차수 + (주문차수 + 빵) 목록
-    return { ...orResult, breadNoList, image: [] };
+    return result;
   } catch (e) {
-    console.log(e);
     return {
       code: 500,
       message: '주문차수 등록 과정에서 문제가 발생했습니다. \n관리자 확인이 필요합니다.',
@@ -181,61 +182,64 @@ export const createWithoutImage = async (
 };
 
 /**
- * 주문차수 등록(이미지 있음)
+ * 주문차수 등록(이미지 O)
  */
 export const createWithImage = async (
-  body: Pick<IOrderRound, 'seq' | 'name' | 'breadNoList' | 'startedAt' | 'endedAt'>,
+  body: Pick<IOrderRound, 'name' | 'breadNoList' | 'startedAt' | 'endedAt'>,
   image: UploadedFile[] | UploadedFile,
 ) => {
   try {
-    // 1. 주문차수 등록 :: orderRound
-    const orResult = await prisma.orderRound.create({
-      data: {
-        name: body.name,
-        startedAt: new Date(body.startedAt),
-        endedAt: new Date(body.endedAt),
-      },
-    });
-
-    // 2. 주문차수에 맞는 빵 목록 등록 :: orderRoundBread
-    const breadNoList = await Promise.all(
-      body.breadNoList.map(async (breadNo) => {
-        const { breadNo: resultBreadNo } = await createOrderRoundBread({
-          seq: orResult.seq,
-          breadNo,
-        });
-
-        return resultBreadNo;
-      }),
-    );
-
-    // 3. 이미지 등록
-    const imgResult = await ImageService.createCloudinary(image);
-    const {
-      url,
-      public_id: publicId,
-      original_filename: name,
-    } = imgResult[0] as { url: string; public_id: string; original_filename: string };
-
-    // 4. 이미지 정보를 데이터베이스에 저장
-    const imageTargetType = await getImageTargetTypeCode(); // return code
-
-    if (url && publicId) {
-      await prisma.image.create({
+    const result = await prisma.$transaction(async (tx) => {
+      // 1. 주문차수 등록 :: orderRound
+      const orResult = await tx.orderRound.create({
         data: {
-          url,
-          publicId,
-          name,
-          imageTargetType,
-          imageTargetNo: orResult.no,
-          order: 1,
+          name: body.name,
+          startedAt: new Date(body.startedAt),
+          endedAt: new Date(body.endedAt),
         },
       });
-    }
 
-    return { ...orResult, breadNoList, image: imgResult[0] };
+      // 2. 주문차수에 맞는 빵 목록 등록 :: orderRoundBread
+      const breadNoList = await Promise.all(
+        body.breadNoList.map(async (breadNo) => {
+          const { breadNo: resultBreadNo } = await createOrderRoundBread(tx, {
+            no: orResult.no,
+            breadNo,
+          });
+
+          return resultBreadNo;
+        }),
+      );
+
+      // 3. 이미지 등록
+      const imgResult = await ImageService.createCloudinary(image);
+      const {
+        url,
+        public_id: publicId,
+        original_filename: name,
+      } = imgResult[0] as { url: string; public_id: string; original_filename: string };
+
+      // 4. 이미지 정보를 데이터베이스에 저장
+      const imageTargetType = await getImageTargetTypeCode(); // return code
+
+      if (url && publicId) {
+        await tx.image.create({
+          data: {
+            url,
+            publicId,
+            name,
+            imageTargetType,
+            imageTargetNo: orResult.no,
+            order: 1,
+          },
+        });
+      }
+
+      return { ...orResult, breadNoList, image: imgResult[0] };
+    });
+
+    return result;
   } catch (e) {
-    console.log(e);
     return {
       code: 500,
       message: '주문차수 등록 과정에서 문제가 발생했습니다. \n관리자 확인이 필요합니다.',
@@ -246,10 +250,13 @@ export const createWithImage = async (
 /**
  * 주문차수 - 빵 매핑 테이블 등록
  */
-const createOrderRoundBread = async ({ seq, breadNo }: { seq: number; breadNo: number }) => {
-  const result = await prisma.orderRoundBread.create({
+const createOrderRoundBread = async (
+  tx: PrismaClient | Prisma.TransactionClient,
+  { no, breadNo }: { no: number; breadNo: number },
+) => {
+  const result = await tx.orderRoundBread.create({
     data: {
-      seq,
+      seq: no,
       breadNo,
     },
   });
@@ -258,50 +265,51 @@ const createOrderRoundBread = async ({ seq, breadNo }: { seq: number; breadNo: n
 };
 
 /**
- * 주문차수 수정 (이미지 없음)
+ * 주문차수 수정 (이미지 X)
  */
 export const updateWithoutImage = async (
-  body: Pick<IOrderRound, 'no' | 'seq' | 'name' | 'breadNoList' | 'startedAt' | 'endedAt'>,
+  body: Pick<IOrderRound, 'no' | 'name' | 'breadNoList' | 'startedAt' | 'endedAt'>,
 ) => {
-  const { no, seq, name, startedAt, endedAt } = body;
+  const { no, name, startedAt, endedAt } = body;
 
   try {
-    // 1. 주문차수 수정 :: orderRound
-    const orResult = await prisma.orderRound.update({
-      where: { no },
-      data: {
-        seq,
-        name,
-        startedAt,
-        endedAt,
-      },
+    const result = await prisma.$transaction(async (tx) => {
+      // 1. 주문차수 수정 :: orderRound
+      const orResult = await tx.orderRound.update({
+        where: { no },
+        data: {
+          name,
+          startedAt,
+          endedAt,
+        },
+      });
+
+      // 2. 주문차수 - 빵  매핑 테이블 수정
+      // 2-1. 기존 빵을 조회하고 다시 수정하는 건 효율이 없으므로 특정 주문차수에 포함된 행은 완전삭제하고 다시 새롭게 등록한다.
+      await tx.orderRoundBread.deleteMany({
+        where: { seq: no },
+      });
+
+      // ************ Postman 테스트를 위해서 일단 강제로 number로 변환
+      body.breadNoList = body.breadNoList.map((breadNo) => Number(breadNo));
+
+      // 2-2. 주문차수에 맞는 빵 목록 등록 :: orderRoundBread
+      const breadNoList = await Promise.all(
+        body.breadNoList.map(async (breadNo) => {
+          const { breadNo: resultBreadNo } = await createOrderRoundBread(tx, {
+            no: body.no,
+            breadNo,
+          });
+
+          return resultBreadNo;
+        }),
+      );
+
+      return { ...orResult, breadNoList, image: [] };
     });
 
-    // 2. 주문차수 - 빵  매핑 테이블 수정
-    // 2-1. 기존 빵을 조회하고 다시 수정하는 건 효율이 없으므로 특정 주문차수에 포함된 행은 완전삭제하고 다시 새롭게 등록한다.
-    await prisma.orderRoundBread.deleteMany({
-      where: { seq },
-    });
-
-    // ************ Postman 테스트를 위해서 일단 강제로 number로 변환
-    body.breadNoList = body.breadNoList.map((breadNo) => Number(breadNo));
-
-    // 2-2. 주문차수에 맞는 빵 목록 등록 :: orderRoundBread
-    const breadNoList = await Promise.all(
-      body.breadNoList.map(async (breadNo) => {
-        const { breadNo: resultBreadNo } = await createOrderRoundBread({
-          seq: body.seq,
-          breadNo,
-        });
-
-        return resultBreadNo;
-      }),
-    );
-
-    return { ...orResult, breadNoList, image: [] };
+    return result;
   } catch (e) {
-    console.log(e);
-
     return {
       code: 500,
       message: '주문차수 수정 과정에서 문제가 발생했습니다. \n관리자 확인이 필요합니다.',
@@ -310,16 +318,13 @@ export const updateWithoutImage = async (
 };
 
 /**
- * 주문차수 수정 (이미지 있음)
+ * 주문차수 수정 (이미지 O)
  */
 export const updateWithImage = async (
-  body: Pick<
-    IOrderRound,
-    'no' | 'seq' | 'name' | 'public_id' | 'breadNoList' | 'startedAt' | 'endedAt'
-  >,
+  body: Pick<IOrderRound, 'no' | 'name' | 'public_id' | 'breadNoList' | 'startedAt' | 'endedAt'>,
   image: UploadedFile[] | UploadedFile,
 ) => {
-  const { no, seq, name, startedAt, endedAt } = body;
+  const { no, name, startedAt, endedAt } = body;
 
   try {
     const result = await prisma.$transaction(async (tx) => {
@@ -327,7 +332,6 @@ export const updateWithImage = async (
       const orResult = await prisma.orderRound.update({
         where: { no },
         data: {
-          seq,
           name,
           startedAt: new Date(startedAt),
           endedAt: new Date(endedAt),
@@ -337,7 +341,7 @@ export const updateWithImage = async (
       // 2. 주문차수 - 빵  매핑 테이블 수정
       // 2-1. 기존 빵을 조회하고 다시 수정하는 건 효율이 없으므로 특정 주문차수에 포함된 행은 완전삭제하고 다시 새롭게 등록한다.
       await prisma.orderRoundBread.deleteMany({
-        where: { seq },
+        where: { seq: orResult.no },
       });
 
       // ************ Postman 테스트를 위해서 일단 강제로 number로 변환
@@ -346,8 +350,8 @@ export const updateWithImage = async (
       // 2-2. 주문차수에 맞는 빵 목록 등록 :: orderRoundBread
       const breadNoList = await Promise.all(
         body.breadNoList.map(async (breadNo) => {
-          const { breadNo: resultBreadNo } = await createOrderRoundBread({
-            seq: body.seq,
+          const { breadNo: resultBreadNo } = await createOrderRoundBread(tx, {
+            no: body.no,
             breadNo,
           });
 
@@ -394,11 +398,8 @@ export const updateWithImage = async (
       return { ...orResult, breadNoList, image: returnImg };
     });
 
-    console.log(result);
     return result;
   } catch (e) {
-    console.log(e);
-
     return {
       code: 500,
       message: '주문차수 수정 과정에서 문제가 발생했습니다. \n관리자 확인이 필요합니다.',
@@ -414,7 +415,6 @@ export const getLatest = async () => {
     const or = await prisma.orderRound.findFirst({
       select: {
         no: true,
-        seq: true,
         name: true,
         startedAt: true,
         endedAt: true,
@@ -425,7 +425,7 @@ export const getLatest = async () => {
         },
       },
       orderBy: {
-        seq: 'desc',
+        no: 'desc',
       },
     });
 
