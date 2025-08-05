48dfa29e449a8bf511d0b8759039e0bf1b515d17
MARUNGs <mi1764@naver.com>
2025-08-05 19:22:02 +0900

    feat(web): 특정 주문차수 조회 API 호출 #147

--- apps/web/src/services/apis.tsx
+++ apps/web/src/services/apis.tsx
@@ -63,3 +63,8 @@ export async function getKakaoCode() {
 export async function getLatest() {
   return client.get(`/order-round/latest`);
 }
+
+/** 특정 주문차수 조회 */
+export async function getOrderRound(no: number) {
+  return client.get(`/order-round/${no}`);
+}
