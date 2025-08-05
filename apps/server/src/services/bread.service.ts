edd8c63a5fb789214b336c4f252d6f5ce83c3d59
silo9506 <ghrn97@gmail.com>
2025-08-05 22:00:15 +0900

    feat(server) : 원산지 정보와 알레르기 정보 추가하도록 수정 #143

--- apps/server/src/services/bread.service.ts
+++ apps/server/src/services/bread.service.ts
@@ -178,7 +178,7 @@ export const getByNo = async (type: ClientType | undefined, no: number) => {
 
 /** 빵 생성 (이미지 없음) */
 export const createWithoutImages = async (
-  body: Pick<Bread, 'name' | 'description' | 'unitPrice' | 'breadStatus'>,
+  body: Pick<Bread, 'name' | 'description' | 'unitPrice' | 'breadStatus' | 'countryOfOrigin'>,
 ) => {
   const result = await prisma.bread.create({
     data: body,
@@ -189,7 +189,7 @@ export const createWithoutImages = async (
 
 /** 빵 생성 (이미지 있음) */
 export const createWithImages = async (
-  body: Pick<Bread, 'name' | 'description' | 'unitPrice' | 'breadStatus'>,
+  body: Pick<Bread, 'name' | 'description' | 'unitPrice' | 'breadStatus' | 'countryOfOrigin'>,
   images: UploadedFile[] | UploadedFile,
 ) => {
   const result = await prisma.$transaction(async (tx) => {
@@ -230,7 +230,9 @@ export const createWithImages = async (
 /** 빵 수정 (이미지 없음) */
 export const updateWithoutImages = async (
   no: number,
-  body: Partial<Pick<Bread, 'name' | 'description' | 'unitPrice' | 'breadStatus'>>,
+  body: Partial<
+    Pick<Bread, 'name' | 'description' | 'unitPrice' | 'breadStatus' | 'countryOfOrigin'>
+  >,
 ) => {
   const result = await prisma.$transaction(async (tx) => {
     const bread = await tx.bread.update({
@@ -257,7 +259,9 @@ export const updateWithoutImages = async (
 /** 빵 수정 (이미지 있음) */
 export const updateWithImages = async (
   no: number,
-  body: Partial<Pick<Bread, 'name' | 'description' | 'unitPrice' | 'breadStatus'>>,
+  body: Partial<
+    Pick<Bread, 'name' | 'description' | 'unitPrice' | 'breadStatus' | 'countryOfOrigin'>
+  >,
   images: UploadedFile[] | UploadedFile,
 ) => {
   const result = await prisma.$transaction(async (tx) => {
