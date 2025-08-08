ddc70731cb53d62bfc178783407ad0ae2b510e8f
MARUNGs <mi1764@naver.com>
2025-08-08 17:59:47 +0900

    chore(web): 아코디언 속성 수정 #174

--- apps/web/src/components/faq/faq-accordion.tsx
+++ apps/web/src/components/faq/faq-accordion.tsx
@@ -7,7 +7,7 @@ function FaqAccordion({ list }: IFaqAccordionProps) {
       <Accordion type="single" collapsible className="lg:w-[60%] mt-10" defaultValue="item-1">
         {list && list.length > 0
           ? list.map((data, i) => (
-              <AccordionItem key={i} value={`item-${i}`}>
+              <AccordionItem key={i} value={`item-${i + 1}`}>
                 <AccordionTrigger className="font-bold">Q. {data.question}</AccordionTrigger>
                 <AccordionContent className="flex flex-col gap-4 text-balance">
                   <p>{data.answer}</p>
