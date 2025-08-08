9b67262d720a4666618292e8d3a989014a02ae0c
MARUNGs <mi1764@naver.com>
2025-08-08 18:02:27 +0900

    chore(web) : cursor-pointer #174

--- apps/web/src/components/faq/faq-accordion.tsx
+++ apps/web/src/components/faq/faq-accordion.tsx
@@ -4,11 +4,13 @@ import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@a
 function FaqAccordion({ list }: IFaqAccordionProps) {
   return (
     <div className="flex items-center- justify-center">
-      <Accordion type="single" collapsible className="lg:w-[60%] mt-10" defaultValue="item-1">
+      <Accordion type="single" collapsible className="lg:w-[60%] mt-10 " defaultValue="item-1">
         {list && list.length > 0
           ? list.map((data, i) => (
               <AccordionItem key={i} value={`item-${i + 1}`}>
-                <AccordionTrigger className="font-bold">Q. {data.question}</AccordionTrigger>
+                <AccordionTrigger className="font-bold cursor-pointer">
+                  Q. {data.question}
+                </AccordionTrigger>
                 <AccordionContent className="flex flex-col gap-4 text-balance">
                   <p>{data.answer}</p>
                 </AccordionContent>
