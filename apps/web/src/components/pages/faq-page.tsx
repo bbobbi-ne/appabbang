e7d031eeb1a1b088e2302904b4e9885ec1b84bd2
MARUNGs <mi1764@naver.com>
2025-08-08 14:26:02 +0900

    feat(web): 자주묻는질문 #170

--- apps/web/src/components/pages/faq-page.tsx
+++ apps/web/src/components/pages/faq-page.tsx
@@ -1,13 +1,38 @@
 /**
  * 자주묻는질문
-  형태 :
-  {
-    category: 'all',
-    question: '____',
-    answer:answer
-  }
  */
 
+import { useEffect, useState } from 'react';
+import FaqMenuButton from '../faq/faq-menu-button';
+import { faqList } from '../faq/faq-data';
+import type { IFaq } from '@/interface/faq-interface';
+import FaqAccordion from '../faq/faq-accordion';
+
 export default function FaqPage() {
-  return <div></div>;
+  const [activeMenu, setActiveMenu] = useState<string>('all'); // 현재 보고 있는 메뉴
+  const [list, setList] = useState<IFaq[]>([]);
+
+  // activeMenu값에 따라 데이터 세팅
+  useEffect(() => {
+    if (activeMenu === 'all') {
+      // 모든 카테고리의 질문/답변 합치기
+      const allFaqs = faqList.flatMap((faq) => faq.data);
+      setList(allFaqs);
+    } else {
+      const selected = faqList.find((faq) => faq.category === activeMenu);
+      setList(selected ? selected.data : []);
+    }
+  }, [activeMenu]);
+
+  console.log(list);
+
+  // 카테고리 명칭 변경
+  const onChangeActiveMenu = (category: string) => setActiveMenu(category);
+
+  return (
+    <div className="flex flex-col">
+      <FaqMenuButton activeMenu={activeMenu} onChangeActiveMenu={onChangeActiveMenu} />
+      <FaqAccordion list={list} />
+    </div>
+  );
 }
