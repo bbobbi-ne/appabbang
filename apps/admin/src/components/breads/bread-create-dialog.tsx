045b428e8bf41db9d78982b44a217246bd910be8
silo9506 <ghrn97@gmail.com>
2025-08-09 21:04:51 +0900

    feat(admin) : dialog 및 form요소 ref제거 #154

--- apps/admin/src/components/breads/bread-create-dialog.tsx
+++ apps/admin/src/components/breads/bread-create-dialog.tsx
@@ -11,12 +11,14 @@ import {
 
 import { useBreadsCreateMutation } from '@/hooks/use-breads';
 import BreadForm from './bread-form';
+import { useState } from 'react';
 
 export function BreadCreateDialog() {
   const { breadsCreateMutation } = useBreadsCreateMutation();
+  const [opne, setOpen] = useState(false);
 
   return (
-    <Dialog>
+    <Dialog open={opne} onOpenChange={(value) => setOpen(value)}>
       <DialogTrigger asChild>
         <Button className="ml-auto">빵 추가하기</Button>
       </DialogTrigger>
@@ -26,12 +28,12 @@ export function BreadCreateDialog() {
         }}
         className="sm:max-w-xl h-fit p-0"
       >
+        <DialogHeader>
+          <DialogTitle>빵 등록</DialogTitle>
+        </DialogHeader>
+        <DialogDescription hidden>메뉴를 등록해주세요</DialogDescription>
         <ScrollArea className="h-[700px] p-6">
-          <DialogHeader>
-            <DialogTitle>빵 등록</DialogTitle>
-          </DialogHeader>
-          <DialogDescription hidden>메뉴를 등록해주세요</DialogDescription>
-          <BreadForm submitFn={breadsCreateMutation} />
+          <BreadForm setOpen={setOpen} submitFn={breadsCreateMutation} />
         </ScrollArea>
       </DialogContent>
     </Dialog>
