de5862c21949ea6b554724765d4ec698977532c5
MARUNGs <mi1764@naver.com>
2025-08-09 19:20:23 +0900

    feat(web) : 구조변경 #181

--- apps/web/src/components/mypage/info-form.tsx
+++ apps/web/src/components/mypage/info-form.tsx
@@ -45,107 +45,109 @@ function InfoForm() {
   const onSubmit: SubmitHandler<CustomerFormSchema> = (data) => {};
 
   return (
-    <Card className="p-10 flex flex-row items-center justify-center w-2/3">
-      <Form {...form}>
-        <form onSubmit={onFormHandler} className="w-2/3 *:m-2 *:has-[.submitBtn]:mt-5">
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field }) => (
-              <FormItem className="w-full m-auto flex flex-row items-center justify-center">
-                <FormLabel htmlFor="name" errorCheck={false} className="w-25">
-                  <span className="text-red-700">*</span> 이름
-                </FormLabel>
-                <FormControl>
-                  <Input
-                    type="text"
-                    id="name"
-                    placeholder="이름 입력"
-                    {...field}
-                    onChange={(e) => field.onChange(e)}
-                    disabled
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+    <div className="w-full flex flex-row items-center justify-center">
+      <Card className="p-10 flex flex-row items-center justify-center w-2/3">
+        <Form {...form}>
+          <form onSubmit={onFormHandler} className="w-2/3 *:m-2 *:has-[.submitBtn]:mt-5">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem className="w-full m-auto flex flex-row items-center justify-center">
+                  <FormLabel htmlFor="name" errorCheck={false} className="w-25">
+                    <span className="text-red-700">*</span> 이름
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      type="text"
+                      id="name"
+                      placeholder="이름 입력"
+                      {...field}
+                      onChange={(e) => field.onChange(e)}
+                      disabled
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-          <FormField
-            control={form.control}
-            name="id"
-            render={({ field }) => (
-              <FormItem className="w-full m-auto flex flex-row items-center justify-center">
-                <FormLabel htmlFor="id" errorCheck={false} className="w-25">
-                  <span className="text-red-700">*</span> 아이디
-                </FormLabel>
-                <FormControl>
-                  <Input
-                    type="text"
-                    id="id"
-                    placeholder="아이디 입력"
-                    {...field}
-                    onChange={(e) => field.onChange(e)}
-                    disabled
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+            <FormField
+              control={form.control}
+              name="id"
+              render={({ field }) => (
+                <FormItem className="w-full m-auto flex flex-row items-center justify-center">
+                  <FormLabel htmlFor="id" errorCheck={false} className="w-25">
+                    <span className="text-red-700">*</span> 아이디
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      type="text"
+                      id="id"
+                      placeholder="아이디 입력"
+                      {...field}
+                      onChange={(e) => field.onChange(e)}
+                      disabled
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-          <FormField
-            control={form.control}
-            name="mobileNumber"
-            render={({ field }) => (
-              <FormItem className="w-full m-auto flex flex-row items-center justify-center">
-                <FormLabel htmlFor="mobileNumber" errorCheck={false} className="w-25">
-                  <span className="text-red-700">*</span> 휴대번호
-                </FormLabel>
-                <FormControl>
-                  <Input
-                    type="text"
-                    id="mobileNumber"
-                    placeholder="휴대번호 입력"
-                    {...field}
-                    onChange={(e) => field.onChange(e)}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="createdAt"
-            render={({ field }) => (
-              <FormItem className="w-full m-auto flex flex-row items-center justify-center">
-                <FormLabel htmlFor="createdAt" errorCheck={false} className="w-25">
-                  <span className="ml-2">가입일자</span>
-                </FormLabel>
-                <FormControl>
-                  <Input
-                    type="text"
-                    id="createdAt"
-                    {...field}
-                    placeholder="가입일자 입력"
-                    onChange={(e) => field.onChange(e)}
-                    disabled
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+            <FormField
+              control={form.control}
+              name="mobileNumber"
+              render={({ field }) => (
+                <FormItem className="w-full m-auto flex flex-row items-center justify-center">
+                  <FormLabel htmlFor="mobileNumber" errorCheck={false} className="w-25">
+                    <span className="text-red-700">*</span> 휴대번호
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      type="text"
+                      id="mobileNumber"
+                      placeholder="휴대번호 입력"
+                      {...field}
+                      onChange={(e) => field.onChange(e)}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="createdAt"
+              render={({ field }) => (
+                <FormItem className="w-full m-auto flex flex-row items-center justify-center">
+                  <FormLabel htmlFor="createdAt" errorCheck={false} className="w-25">
+                    <span className="ml-2">가입일자</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      type="text"
+                      id="createdAt"
+                      {...field}
+                      placeholder="가입일자 입력"
+                      onChange={(e) => field.onChange(e)}
+                      disabled
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-          <div>
-            <Button type="submit" className="rounded-2xl h-10 w-full font-bold submitBtn">
-              수정
-            </Button>
-          </div>
-        </form>
-      </Form>
-    </Card>
+            <div>
+              <Button type="submit" className="rounded-2xl h-10 w-full font-bold submitBtn">
+                수정
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </Card>
+    </div>
   );
 }
 
--- apps/web/src/components/pages/info-page.tsx
+++ apps/web/src/components/pages/info-page.tsx
@@ -1,11 +1,6 @@
-import { Card } from '@appabbang/ui';
 import InfoForm from '../mypage/info-form';
 
 // 마이페이지 정보 페이지
 export default function InfoPage() {
-  return (
-    <div className="w-full flex flex-row items-center justify-center">
-      <InfoForm />
-    </div>
-  );
+  return <InfoForm />;
 }
