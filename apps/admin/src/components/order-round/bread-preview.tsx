8071513ef5222f1199394b13da1145a04c109d97
silo9506 <ghrn97@gmail.com>
2025-08-09 21:05:23 +0900

    feat(admin): order-round를 위한 빵미리보기 및 이미지 업로드 컴포넌트 제작 #154

---
+++ apps/admin/src/components/order-round/bread-preview.tsx
@@ -0,0 +1,42 @@
+import { useBreadsDetailQuery } from '@/hooks/use-breads';
+import { formatKR } from '@/utils/format';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@appabbang/ui';
+
+function BreadPreview({ no }: { no: number }) {
+  const { data, isSuccess, isLoading } = useBreadsDetailQuery(no);
+
+  if (isLoading) return;
+
+  return (
+    <Card className="fixed right-0 translate-x-[100%] w-[300px] ">
+      <CardHeader>
+        <CardTitle className="text-center text-sm">{data!.name}</CardTitle>
+        <CardDescription hidden>빵에대한 상세설명입니다.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <img
+          alt={data?.name}
+          className="h-full w-full rounded-lg object-fill"
+          src={data?.images[0]?.url || ''}
+        />
+
+        <div className="space-y-1">
+          <div className="flex text-xs">
+            <p className="flex-2/5">가격 : </p>
+            <p className="flex-4/5">{formatKR(data!.unitPrice)}원</p>
+          </div>
+          <div className="flex text-xs">
+            <p className="flex-2/5">알레르기 정보 : </p>
+            <p className="flex-4/5">{data!.allergyInfo}</p>
+          </div>
+          <div className="flex text-xs">
+            <p className="flex-2/5">원산지 정보 : </p>
+            <p className="flex-4/5">{data!.countryOfOrigin}</p>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default BreadPreview;
---
+++ apps/admin/src/components/order-round/image-upload.tsx
@@ -0,0 +1,67 @@
+import { useOrderRoundImgDeleteMutation } from '@/hooks/use-order-round';
+import { Input } from '@appabbang/ui';
+import { useRef } from 'react';
+import type { ControllerRenderProps } from 'react-hook-form';
+
+export function ImageUpload({
+  field,
+  no,
+}: {
+  field: ControllerRenderProps<any, any>;
+  no?: number;
+}) {
+  const { value, onChange } = field;
+  const inputRef = useRef<HTMLInputElement>(null);
+  const { orderRoundImgDeleteMutation } = useOrderRoundImgDeleteMutation();
+  const handleInputClick = () => {
+    inputRef.current?.click();
+  };
+
+  const handleFiles = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file && file.type.startsWith('image/')) {
+      onChange(file);
+    }
+    if (inputRef.current) {
+      inputRef.current.value = '';
+    }
+  };
+
+  const removeImage = () => {
+    if (value instanceof File) {
+      inputRef.current?.click();
+      return;
+    }
+    orderRoundImgDeleteMutation({ no: no!, publicId: value.publicId });
+    onChange(null);
+  };
+
+  return (
+    <div className="space-y-2">
+      <Input
+        type="file"
+        accept="image/*"
+        onChange={handleFiles}
+        value={undefined}
+        className="hidden"
+        ref={inputRef}
+      />
+      {!value ? (
+        <div
+          onClick={handleInputClick}
+          className="flex items-center justify-center h-40 border-2 border-dashed rounded cursor-pointer text-muted-foreground hover:bg-muted/50 transition"
+        >
+          이미지 파일을 추가해주세요
+        </div>
+      ) : (
+        <div onClick={removeImage} className="border rounded overflow-hidden cursor-pointer">
+          <img
+            src={value instanceof File ? URL.createObjectURL(value) : value.url}
+            alt="preview"
+            className="w-full h-40 rounded object-fill"
+          />
+        </div>
+      )}
+    </div>
+  );
+}
