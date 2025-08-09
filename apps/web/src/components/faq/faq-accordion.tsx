71142aeaa1f411e8ddce9cff2378ff53d6c39f60
MARUNGs <mi1764@naver.com>
2025-08-09 16:04:09 +0900

    feat(web) : FAQ, 회원탈퇴 모달 개발 #176

--- apps/web/src/components/faq/faq-accordion.tsx
+++ apps/web/src/components/faq/faq-accordion.tsx
@@ -1,7 +1,11 @@
 import type { IFaqAccordionProps } from '@/interface/faq-interface';
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@appabbang/ui';
+import FaqUnregisterDialog from './faq-unregister-dialog';
 
 function FaqAccordion({ list }: IFaqAccordionProps) {
+  // 회원가입
+  const onClick = () => {};
+
   return (
     <div className="flex items-center- justify-center">
       <Accordion type="single" collapsible className="lg:w-[60%] mt-10 " defaultValue="item-1">
@@ -12,7 +16,27 @@ function FaqAccordion({ list }: IFaqAccordionProps) {
                   Q. {data.question}
                 </AccordionTrigger>
                 <AccordionContent className="flex flex-col gap-4 text-balance">
-                  <p>{data.answer}</p>
+                  {data.answer.includes('탈퇴') ? (
+                    <p>
+                      {data.answer.split(/(여기)/).map((part, index) =>
+                        part === '여기' ? (
+                          <FaqUnregisterDialog>
+                            <span
+                              key={index}
+                              onClick={onClick}
+                              className="text-blue-500 underline cursor-pointer"
+                            >
+                              {part}
+                            </span>
+                          </FaqUnregisterDialog>
+                        ) : (
+                          <span key={index}>{part}</span>
+                        ),
+                      )}
+                    </p>
+                  ) : (
+                    <p>{data.answer}</p>
+                  )}
                 </AccordionContent>
               </AccordionItem>
             ))
