af112c5703dfca658974cf36ee53f6d645c79f46
silo9506 <ghrn97@gmail.com>
2025-08-09 21:07:53 +0900

    feat(admin) : order-round를 위한 api 함수 및 쿼리훅 추가 조회 등록 수정 삭제(미구현)  #154

--- apps/admin/src/hooks/use-order-round.ts
+++ apps/admin/src/hooks/use-order-round.ts
@@ -1,12 +1,61 @@
-import { getOrderRoundsList } from '@/service/order-round-api';
-import { useQuery } from '@tanstack/react-query';
+import {
+  createOrderRound,
+  deleteOrderRoundImg,
+  getDetailOrderRoundsList,
+  getOrderRoundsList,
+  updateOrderRound,
+} from '@/service/order-round-api';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 export function useOrderRoundsQuery() {
   return useQuery({
-    queryKey: ['order-round'],
+    queryKey: ['order-rounds'],
     queryFn: getOrderRoundsList,
     staleTime: Infinity,
     retry: 1,
     select: (res) => res.data,
   });
+}
+export function useOrderRoundDetailQuery(no: number) {
+  return useQuery({
+    queryKey: ['order-round', no],
+    queryFn: () => getDetailOrderRoundsList(no),
+    staleTime: Infinity,
+    retry: 1,
+    select: (res) => res.data,
+  });
+}
+export function useOrderRoundCreateMutation() {
+  const queryClient = useQueryClient();
+  const { mutateAsync, error, isError, isSuccess, isPending } = useMutation({
+    mutationFn: createOrderRound,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['order-rounds'] });
+    },
+  });
+
+  return { orderRoundCreateMutation: mutateAsync, isError, error, isSuccess, isPending };
+}
+
+export function useOrderRoundImgDeleteMutation() {
+  const queryClient = useQueryClient();
+  const { mutateAsync, error, isError, isSuccess, isPending } = useMutation({
+    mutationFn: deleteOrderRoundImg,
+    onSuccess: (_, variables) => {
+      queryClient.invalidateQueries({ queryKey: ['order-rounds'] });
+      queryClient.invalidateQueries({ queryKey: ['order-round', { no: variables.no }] });
+    },
+  });
+  return { orderRoundImgDeleteMutation: mutateAsync, isError, error, isSuccess, isPending };
+}
+export function useOrderRoundUpdateMutation() {
+  const queryClient = useQueryClient();
+  const { mutateAsync, error, isError, isSuccess, isPending } = useMutation({
+    mutationFn: updateOrderRound,
+    onSuccess: (_, variables) => {
+      queryClient.invalidateQueries({ queryKey: ['order-rounds'] });
+      queryClient.invalidateQueries({ queryKey: ['order-round', { no: variables.no }] });
+    },
+  });
+  return { orderRoundUpdateMutation: mutateAsync, isError, error, isSuccess, isPending };
 }
--- apps/admin/src/service/order-round-api.ts
+++ apps/admin/src/service/order-round-api.ts
@@ -1,6 +1,7 @@
 import { OrderRound } from '@/api/OrderRound';
 import { CustomHttpClient } from './instance';
 import { toast } from 'sonner';
+import type { OrderRoundCreatePayload, OrderRoundUpdatePayload } from '@/api/data-contracts';
 
 const orderRoundsApi = new OrderRound(new CustomHttpClient());
 
@@ -14,6 +15,96 @@ export const getOrderRoundsList = async () => {
   } catch (error: any) {
     const message = error.data.message || '주문차수를 불러오는데 실패했습니다.';
     toast.error('주문차수를 불러오는데 실패했습니다.', {
+      description: message,
+    });
+    throw new Error(message);
+  }
+};
+export const getDetailOrderRoundsList = async (no: number) => {
+  try {
+    const response = await orderRoundsApi.orderRoundDetail(no);
+    // toast.success('주문차수 상세 조회에 성공했습니다.');
+    return {
+      data: response.data,
+    };
+  } catch (error: any) {
+    const message = error.data.message || '주문차수를 조회하는데 실패했습니다.';
+    toast.error('주문차수를 조회하는데 실패했습니다.', {
+      description: message,
+    });
+    throw new Error(message);
+  }
+};
+
+export const createOrderRound = async ({
+  formData,
+}: {
+  formData: OrderRoundCreatePayload | FormData;
+}) => {
+  console.log(formData, '주문차수생성 실행됨');
+
+  try {
+    const response = await orderRoundsApi.orderRoundCreate(formData as OrderRoundCreatePayload);
+    // toast.success('주문차수 생성에 성공했습니다.');
+    return {
+      data: response.data,
+    };
+  } catch (error: any) {
+    const message = error.data.message || '주문차수 생성을 실패했습니다.';
+    toast.error('주문차수 생성을 실패했습니다.', {
+      description: message,
+    });
+    throw new Error(message);
+  }
+};
+export const updateOrderRound = async ({
+  formData,
+  no,
+}: {
+  formData: OrderRoundUpdatePayload | FormData;
+  no: number;
+}) => {
+  console.log(formData, no, '주문차수수정 실행됨');
+  try {
+    const response = await orderRoundsApi.orderRoundUpdate(no, formData as OrderRoundUpdatePayload);
+    // toast.success('주문차수 업데이트에 성공했습니다.');
+    return {
+      data: response.data,
+    };
+  } catch (error: any) {
+    const message = error.data.message || '주문차수 업데이트를 실패했습니다.';
+    toast.error('주문차수 업데이트를 실패했습니다.', {
+      description: message,
+    });
+    throw new Error(message);
+  }
+};
+
+export const deleteOrderRound = async (no: number) => {
+  try {
+    const response = await orderRoundsApi.orderRoundDetail(no);
+    // toast.success('주문차수 삭제에 성공했습니다.');
+    return {
+      data: response.data,
+    };
+  } catch (error: any) {
+    const message = error.data.message || '주문차수 삭제를 실패했습니다.';
+    toast.error('주문차수 삭제를 실패했습니다.', {
+      description: message,
+    });
+    throw new Error(message);
+  }
+};
+export const deleteOrderRoundImg = async ({ publicId, no }: { publicId: string; no: number }) => {
+  try {
+    const response = await orderRoundsApi.imageDelete({ publicId });
+    // toast.success('주문차수 이미지 제거에 성공했습니다.');
+    return {
+      data: response.data,
+    };
+  } catch (error: any) {
+    const message = error.data.message || '주문차수 이미지 제거를 실패했습니다.';
+    toast.error('주문차수 이미지 제거를 실패했습니다.', {
       description: message,
     });
     throw new Error(message);
