974ce9ee7365a4200d238e098b8b3af9bd88acfe
MARUNGs <mi1764@naver.com>
2025-08-09 19:12:12 +0900

    feat(web) : 내정보수정 페이지, 내정보수정 form, validate 구현 #181

--- apps/web/src/components/pages/info-page.tsx
+++ apps/web/src/components/pages/info-page.tsx
@@ -1,4 +1,11 @@
+import { Card } from '@appabbang/ui';
+import InfoForm from '../mypage/info-form';
+
 // 마이페이지 정보 페이지
 export default function InfoPage() {
-  return <div className="w-full">내정보</div>;
+  return (
+    <div className="w-full flex flex-row items-center justify-center">
+      <InfoForm />
+    </div>
+  );
 }
