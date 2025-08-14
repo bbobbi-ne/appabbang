1e2c01b7dfc84f625fdf058e7292b30f22dec8a1
MARUNGs <mi1764@naver.com>
2025-08-14 13:26:19 +0900

    feat(web) : async - async 개선 #196

--- apps/web/src/services/customer-apis.ts
+++ apps/web/src/services/customer-apis.ts
@@ -43,62 +43,66 @@ export async function createCustomer(
   },
   set: (accountToken: string) => void,
 ) {
-  client
-    .post('/customers', data)
-    .then((response) => {
-      if (response.status === 201) {
-        // accessToken을 상태관리에 저장
-        const accessToken = response.data.accessToken;
-        sessionStorage.setItem('accessToken', accessToken);
-        set(accessToken);
+  try {
+    const response = await client.post('/customers', data);
 
-        // success message
-        addToast({
-          type: 'success',
-          message: `${response.data.data.name}님, 환영합니다!`,
-        });
+    if (response.status === 201) {
+      // accessToken을 상태관리에 저장
+      const accessToken = response.data.accessToken;
+      sessionStorage.setItem('accessToken', accessToken);
+      set(accessToken);
 
-        // 메인페이지 이동
-        setTimeout(() => {
-          window.location.href = '/';
-        }, 1500);
-      } else throw new CustomError(500, 'fail');
-    })
-    .catch((e) => {
-      e.response.status === 500 &&
-        addToast({
-          message: e.response.data.error.message,
-          type: 'error',
-        });
+      // success message
+      addToast({
+        type: 'success',
+        message: `${response.data.data.name}님, 환영합니다!`,
+      });
+
+      // 메인페이지 이동
+      setTimeout(() => {
+        window.location.href = '/';
+      }, 1500);
+    } else {
+      throw new CustomError(500, 'fail');
+    }
+  } catch (e: any) {
+    addToast({
+      type: 'error',
+      message: e.response.data.error.message,
     });
+  }
 }
 
 /**
  * 로그아웃
  */
 export const logout = async (accessToken: string, reset: (accessToken: string) => void) => {
-  client
-    .post('/customers/logout', {}, { headers: { Authorization: `Bearer ${accessToken}` } })
-    .then((response) => {
-      if (response.status === 204) {
-        reset(accessToken);
+  try {
+    const response = await client.post(
+      '/customers/logout',
+      {},
+      { headers: { Authorization: `Bearer ${accessToken}` } },
+    );
 
-        addToast({
-          type: 'success',
-          message: '정상적으로 로그아웃되었습니다. 다음에 다시 만나요!',
-        });
+    if (response.status === 204) {
+      reset(accessToken);
 
-        // 메인페이지 이동
-        setTimeout(() => {
-          window.location.href = '/';
-        }, 1500);
-      }
-    })
-    .catch((e) => {
-      e.response.status === 500 &&
-        addToast({
-          type: 'error',
-          message: e.response.data.error.message,
-        });
+      addToast({
+        type: 'success',
+        message: '정상적으로 로그아웃되었습니다. 다음에 다시 만나요!',
+      });
+
+      // 메인페이지 이동
+      setTimeout(() => {
+        window.location.href = '/';
+      }, 1500);
+    } else {
+      throw new CustomError(500, 'fail');
+    }
+  } catch (e: any) {
+    addToast({
+      type: 'error',
+      message: e.response.data.error.message,
     });
+  }
 };
