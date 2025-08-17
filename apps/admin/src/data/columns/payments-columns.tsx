047d186bd36977a80a923532fc9a0deba9a93392
silo9506 <ghrn97@gmail.com>
2025-08-17 18:28:31 +0900

    feat(admin) : sort옵션 추가 및 filterFn추가 #214

--- apps/admin/src/data/columns/payments-columns.tsx
+++ apps/admin/src/data/columns/payments-columns.tsx
@@ -10,10 +10,10 @@ import {
 import { createColumnHelper, type ColumnDef } from '@tanstack/react-table';
 import { SortAsc, SortDesc } from 'lucide-react';
 import { formatIsoToDateTime, formatCurrencyKR } from '@/utils/format';
-
 import { usePaidUpdateMutation } from '@/hooks/use-payment';
 import { RefundDialog } from '@/components/payments/refund-dialog';
 import { useGetOrderStatusQuery } from '@/hooks/use-common-code';
+import { renderSortButton } from '@/components/ui/rebder-sort-button';
 
 export type PaymentsListItem = PaymentsListData[number];
 
@@ -21,63 +21,83 @@ export const paymentsColumns = () => {
   const columnHelper = createColumnHelper<PaymentsListItem>();
   const { paidUpdateMutation } = usePaidUpdateMutation();
   const { data: orderStatus } = useGetOrderStatusQuery();
+
   const columns: ColumnDef<PaymentsListItem, any>[] = [
-    columnHelper.display({
-      id: 'cell-no',
+    columnHelper.accessor('no', {
       maxSize: 1,
-      header: ({ column }) => {
-        const isSorted = column.getIsSorted() === 'asc';
-        return (
-          <Button
-            className={`p-0 ${isSorted ? 'text-blue-500' : ''} hover:text-primary gap-0`}
-            variant="ghost"
-            onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-          >
-            {isSorted ? <SortAsc /> : <SortDesc />} 번호
-          </Button>
-        );
-      },
+      header: ({ column }) => renderSortButton(column, '번호'),
       cell: (info) => {
         const index = info.table.getPrePaginationRowModel().rows.length - info.row.index;
         return <p className="text-center">{index}</p>;
       },
     }),
     columnHelper.accessor('order.orderNumber', {
-      maxSize: 2,
-      header: ({ column }) => <p>주문번호</p>,
-      cell: (info) => {
-        return <p>{info.getValue()}</p>;
-      },
+      maxSize: 5,
+      header: () => <p>주문번호</p>,
+      cell: (info) => <p>{info.getValue()}</p>,
     }),
-    columnHelper.accessor('order.orderStatusName', {
+    columnHelper.accessor('order.orderStatus', {
       maxSize: 1,
-      header: ({ column }) => (
-        <Button className="p-0" variant="ghost">
-          주문상태
-        </Button>
-      ),
+      header: ({ column }) => {
+        const rawValue = column.getFilterValue();
+        const value = typeof rawValue === 'string' ? rawValue : 'all';
+
+        return (
+          <Select
+            value={value}
+            onValueChange={(val) => column.setFilterValue(val === 'all' ? undefined : val)}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="주문상태" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="all">전체</SelectItem>
+              {orderStatus?.map(({ code, name }) => (
+                <SelectItem key={code} value={code}>
+                  {name}
+                </SelectItem>
+              ))}
+            </SelectContent>
+          </Select>
+        );
+      },
       cell: (info) => {
-        return <p className="text-center">{info.getValue()}</p>;
+        const code = info.getValue();
+        const status = orderStatus?.find((item) => item.code === code)?.name || '';
+        return <p className="text-center">{status}</p>;
+      },
+      filterFn: (row, columnId, filterValue) => {
+        if (!filterValue || filterValue === 'all') return true;
+        return row.getValue(columnId) === filterValue;
       },
     }),
     columnHelper.accessor('isPaid', {
-      maxSize: 3,
-      header: ({ column }) => (
-        <Button className="p-0" variant="ghost">
-          결제완료여부
-        </Button>
-      ),
+      maxSize: 1,
+      header: ({ column }) => {
+        const rawValue = column.getFilterValue();
+        const value = typeof rawValue === 'string' ? rawValue : 'all';
+        return (
+          <Select
+            value={value}
+            onValueChange={(val) => column.setFilterValue(val === 'all' ? undefined : val)}
+          >
+            <SelectTrigger>
+              <SelectValue placeholder="결제완료 여부" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="all">전체</SelectItem>
+              <SelectItem value="true">완료</SelectItem>
+              <SelectItem value="false">미완료</SelectItem>
+            </SelectContent>
+          </Select>
+        );
+      },
       cell: (info) => {
         const value = info.getValue() ? '완료' : '미완료';
         const no = info.row.original.no;
         const disabledCodes = orderStatus?.filter((item) =>
           ['접수요청', '취소요청'].includes(item.name),
         );
-
-        const rowOrderStatus = disabledCodes?.find(
-          (item) => item.code === info.row.original.order.orderStatus,
-        );
-
         return (
           <Select
             disabled={
@@ -85,11 +105,8 @@ export const paymentsColumns = () => {
             }
             value={value}
             onValueChange={(selectValue) => {
-              const isPaid = selectValue === '완료' ? true : false;
-              paidUpdateMutation({
-                no,
-                data: { isPaid, orderNo: no },
-              });
+              const isPaid = selectValue === '완료';
+              paidUpdateMutation({ no, data: { isPaid, orderNo: no } });
             }}
           >
             <SelectTrigger>
@@ -102,95 +119,70 @@ export const paymentsColumns = () => {
           </Select>
         );
       },
-    }),
-
-    columnHelper.accessor('order.totalPrice', {
-      maxSize: 6,
-      header: ({ column }) => (
-        <Button className="p-0" variant="ghost">
-          결제금액
-        </Button>
-      ),
-      cell: (info) => {
-        return <p>{formatCurrencyKR(info.getValue())}원</p>;
+      filterFn: (row, columnId, filterValue) => {
+        if (filterValue === undefined) return true;
+        const rowValue = row.getValue(columnId);
+        return String(rowValue) === filterValue;
       },
+    }),
+    columnHelper.accessor('order.totalPrice', {
+      maxSize: 2,
+      header: ({ column }) => renderSortButton(column, '결제금액'),
+      cell: (info) => <p>{formatCurrencyKR(info.getValue())}원</p>,
     }),
     columnHelper.accessor('createdAt', {
       maxSize: 3,
-      header: ({ column }) => (
-        <Button className="p-0" variant="ghost">
-          주문일시
-        </Button>
-      ),
-      cell: (info) => {
-        return <p className="text-center">{formatIsoToDateTime(info.getValue())}</p>;
-      },
+      header: ({ column }) => renderSortButton(column, '주문일시'),
+      cell: (info) => <p>{formatIsoToDateTime(info.getValue())}</p>,
     }),
     columnHelper.accessor('paidConfirmedAt', {
       maxSize: 3,
-      header: ({ column }) => (
-        <Button className="p-0" variant="ghost">
-          입금확인일시
-        </Button>
-      ),
+      header: ({ column }) => renderSortButton(column, '입금확인일시'),
       cell: (info) => {
         const value = info.getValue() === null ? '미입금' : formatIsoToDateTime(info.getValue());
-
-        return <p className="text-center">{value}</p>;
+        return <p>{value}</p>;
+      },
+      sortingFn: (rowA, rowB, columnId) => {
+        const a = rowA.getValue(columnId) ? new Date(rowA.getValue(columnId)).getTime() : 0;
+        const b = rowB.getValue(columnId) ? new Date(rowB.getValue(columnId)).getTime() : 0;
+        return a - b;
       },
     }),
     columnHelper.accessor('bankCodeName', {
-      maxSize: 3,
-      header: ({ column }) => (
+      maxSize: 1,
+      header: () => (
         <Button className="p-0" variant="ghost">
           은행명
         </Button>
       ),
-      cell: (info) => {
-        return <p className="text-center">{info.getValue()}</p>;
-      },
+      cell: (info) => <p>{info.getValue()}</p>,
     }),
     columnHelper.accessor('accountNumber', {
       maxSize: 3,
-      header: ({ column }) => (
+      header: () => (
         <Button className="p-0" variant="ghost">
           계좌번호
         </Button>
       ),
-      cell: (info) => {
-        return <p className="text-center">{info.getValue()}</p>;
-      },
+      cell: (info) => <p>{info.getValue()}</p>,
     }),
     columnHelper.accessor('accountHolderName', {
-      maxSize: 3,
-      header: ({ column }) => (
+      maxSize: 1,
+      header: () => (
         <Button className="p-0" variant="ghost">
           예금주명
         </Button>
       ),
-      cell: (info) => {
-        return <p className="text-center">{info.getValue()}</p>;
-      },
+      cell: (info) => <p>{info.getValue()}</p>,
     }),
     columnHelper.display({
       id: 'refundDetails',
       maxSize: 1,
-      header: ({ column }) => {
-        return <p>취소/환불정보</p>;
-      },
+      header: () => <p>취소/환불정보</p>,
       cell: (info) => {
         const refundCode = ['50', '51', '52'];
-
-        const refund = !!refundCode.includes(info.row.original.order.orderStatus);
-        return (
-          <>
-            {refund ? (
-              <RefundDialog no={info.row.original.no} />
-            ) : (
-              <Button disabled={true}>확인</Button>
-            )}
-          </>
-        );
+        const refund = refundCode.includes(info.row.original.order.orderStatus);
+        return refund ? <RefundDialog no={info.row.original.no} /> : <Button disabled>확인</Button>;
       },
     }),
   ];
