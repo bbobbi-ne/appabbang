67b0febbcb6eae0270c327c17e6e029e00853c6f
jieun-c <flffl1231@naver.com>
2025-08-24 13:24:31 +0900

    style(web): 로그인 간단 ui 수정 #227

--- apps/web/src/components/pages/login-page.tsx
+++ apps/web/src/components/pages/login-page.tsx
@@ -1,16 +1,16 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@appabbang/ui';
-import LoginForm from '../login/login-form';
-import KakaoLoginButton from '../login/kakao-login-button';
+import LoginForm from '@/components/login/login-form';
+import KakaoLoginButton from '@/components/login/kakao-login-button';
 
 function LoginPage() {
   return (
-    <div className="flex flex-col min-h-screen items-center justify-center gap-4 p-24">
-      <Card className="w-full max-w-sm">
+    <div className="p-4">
+      <Card className="w-full max-w-sm mx-auto">
         <CardHeader>
-          <div className="flex items-center justify-between">
-            <CardTitle className="inline">사용자 로그인</CardTitle>
-          </div>
-          <CardDescription>로그인 후 사용이 가능합니다!</CardDescription>
+          <CardTitle className="text-center text-2xl">로그인</CardTitle>
+          <CardDescription className="text-center text-xs">
+            환영합니다! 아빠빵 서비스를 이용해보세요.
+          </CardDescription>
         </CardHeader>
         <CardContent className="space-y-2">
           <LoginForm />
--- apps/web/src/routes/login.tsx
+++ apps/web/src/routes/login.tsx
@@ -7,9 +7,5 @@ import { createFileRoute } from '@tanstack/react-router';
 import LoginPage from '@/components/pages/login-page';
 
 export const Route = createFileRoute('/login')({
-  component: Login,
+  component: LoginPage,
 });
-
-function Login() {
-  return <LoginPage />;
-}
