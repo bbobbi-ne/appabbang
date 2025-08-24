e5fdfe88b062ab5eac9929219bd9fd88040c021a
silo9506 <ghrn97@gmail.com>
2025-08-24 13:03:56 +0900

    fix(admin) : 테이블 높이를 고정 -> max-h로 수정 #231

--- apps/admin/src/components/ui/table-container.tsx
+++ apps/admin/src/components/ui/table-container.tsx
@@ -81,8 +81,8 @@ export function TableContainer<TData>({
 
       {/* 테이블 본문 */}
       <CardContent>
-        <ScrollArea className="h-[550px] w-full border-1 rounded-lg">
-          <div className="min-w-[1000px]">
+        <ScrollArea className="border-1 rounded-lg">
+          <div className="max-h-[550px] w-full ">
             <Table>
               {/* 컬럼 헤더 */}
               <TableHeader className="sticky z-5 top-0 bg-background shadow border-b-0">
