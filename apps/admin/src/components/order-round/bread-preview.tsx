0755beaf47c20672c8e3283025467236915e5c56
silo9506 <ghrn97@gmail.com>
2025-08-24 12:55:11 +0900

    feat(admin) : 빵사진 없음 기본이미지 변경 #231

--- apps/admin/src/components/order-round/bread-preview.tsx
+++ apps/admin/src/components/order-round/bread-preview.tsx
@@ -9,17 +9,16 @@ function BreadPreview({ no }: { no: number }) {
 
   return (
     <Card className="fixed right-0 translate-x-[100%] w-[300px] ">
-      <CardHeader>
+      <CardHeader className="pb-0">
         <CardTitle className="text-center text-sm">{data!.name}</CardTitle>
         <CardDescription hidden>빵에대한 상세설명입니다.</CardDescription>
       </CardHeader>
       <CardContent>
         <img
           alt={data?.name}
-          className="h-full w-full rounded-lg object-fill"
-          src={data?.images[0]?.url || ''}
+          className="h-full w-full max-h-44 rounded-lg object-fill "
+          src={data?.images[0]?.url || '/images/no_image.png'}
         />
-
         <div className="space-y-1">
           <div className="flex text-xs">
             <p className="flex-2/5">가격 : </p>
