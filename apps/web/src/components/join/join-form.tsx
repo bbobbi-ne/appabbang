0b45848208744b29e1a90229db2c75e3240fbb4b
MARUNGs <mi1764@naver.com>
2025-08-28 19:45:54 +0900

    chore(web) : 회원가입 mutation 재사용성 개선 #249

--- apps/web/src/components/join/join-form.tsx
+++ apps/web/src/components/join/join-form.tsx
@@ -43,10 +43,6 @@ export default function JoinForm() {
   const [showCode, setShowCode] = useState<boolean>(false);
   const [check, setCheck] = useState<boolean>(false);
   const { code, set: setEmailCode, reset: resetEmailCode } = useEmailCodeStore();
-  const [_, setEmail] = useState<string>('');
-  const createCustomer = useCreateCustomerMutation(() => resetEmailCode());
-
-  const getEmail = useGetEmailMutation();
 
   // 폼 선언
   const form = useForm<JoinSchemaType>({
@@ -69,9 +65,11 @@ export default function JoinForm() {
     },
   });
 
-  const getCheckId = useGetCheckIdMutation(form);
-  const sendEmail = useSendEmailMutation(setEmailCode, setShowCode, form);
-  const compareEmailCode = useCompareEmailCodeMutation(form, setCheck, resetEmailCode);
+  const createCustomer = useCreateCustomerMutation(); // 회원가입
+  const getEmail = useGetEmailMutation(); // 이메일 가져오기
+  const getCheckId = useGetCheckIdMutation(); // 아이디 중복체크 :: 존재하는 아이디 찾기
+  const sendEmail = useSendEmailMutation(); // 입력한 이메일로 인증코드 전송
+  const compareEmailCode = useCompareEmailCodeMutation(); // 이메일 인증번호 비교
 
   /**************************************************************************************************************/
 
@@ -115,8 +113,11 @@ export default function JoinForm() {
         return;
       }
 
-      setEmail(email);
-      email.trim() !== '' && sendEmail.mutateAsync({ email }); // 이메일
+      const result = await sendEmail.mutateAsync({ email }); // 이메일
+
+      setEmailCode(result.code);
+      form.clearErrors('email');
+      setShowCode(true);
     } catch (e) {
       addToast({ type: 'error', message: '이메일 인증 과정에서 문제가 발생했습니다.' });
     }
@@ -128,7 +129,15 @@ export default function JoinForm() {
 
     // 이메일 인증코드 비교
     try {
-      await compareEmailCode.mutateAsync({ code: value, hashedCode: code });
+      const result = await compareEmailCode.mutateAsync({ code: value, hashedCode: code });
+      if (Number(result.code) === 200) {
+        form.clearErrors('code');
+        setCheck(true);
+        resetEmailCode();
+      } else {
+        form.setError('code', { type: 'value', message: '인증번호가 일치하지 않습니다.' });
+        setCheck(false);
+      }
     } catch (e) {
       addToast({ type: 'error', message: '이메일 인증코드 비교 과정에서 오류가 발생했습니다.' });
     }
@@ -150,7 +159,14 @@ export default function JoinForm() {
     }
 
     try {
-      await getCheckId.mutateAsync({ id });
+      const result = await getCheckId.mutateAsync({ id });
+
+      if (result.id) {
+        form.setError('id', { type: 'value', message: '이미 존재하는 아이디입니다.' });
+        form.setFocus('id');
+      } else {
+        form.clearErrors('id');
+      }
     } catch (e) {
       addToast({ type: 'error', message: '아이디 중복체크 과정에서 오류가 발생했습니다.' });
     }
@@ -185,7 +201,14 @@ export default function JoinForm() {
    */
   const onSubmit = async (data: CustomersCreatePayload) => {
     try {
-      await createCustomer.mutateAsync(data);
+      const { data: result } = await createCustomer.mutateAsync(data);
+
+      if (result) {
+        resetEmailCode();
+        debugger;
+        addToast({ type: 'success', message: `${result.name}님, 환영합니다!` });
+        setTimeout(() => (window.location.href = '/'), 1500);
+      }
     } catch (error: any) {
       addToast({ type: 'error', message: error.response.data.error.message });
     }
