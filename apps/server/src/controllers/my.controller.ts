147933e24deedf2332fafe7a413da65dd44e6ee1
MARUNGs <mi1764@naver.com>
2025-08-29 19:42:22 +0900

    feat(web, server) : 마이페이지 - 내정보수정 email 항목 추가에 따른 api 개선 #248 * summary section과 myInfo section의 공통으로 호출하는 api를 분리하였음. * email 을 조회하는 api 리턴값 변경에 따라, 공통으로 호출하는 각 화면의 분기처리를 개선하였음.

--- apps/server/src/controllers/my.controller.ts
+++ apps/server/src/controllers/my.controller.ts
@@ -8,9 +8,20 @@ export const getInfo = async (req: Request, res: Response) => {
   if (!req.user) throw AppError.unauthorized('고객정보를 조회할 수 없습니다.');
 
   // 고객 상세정보 조회 + 고객 보유 쿠폰 조회
-  const { customer, coupon } = await myService.getMyInfo(req.user.no);
-  const totalAmount = await myService.getOrderAccumulatedAmount(req.user.no);
-  res.status(200).json({ customer, coupon, totalAmount });
+  const customer = await myService.getMyInfo(req.user.no);
+  res.status(200).json(customer);
+};
+
+/** 내 정보 + 주문 누적금액 + 총 보유 쿠폰 수 조회 */
+export const getLayoutInfo = async (req: Request, res: Response) => {
+  if (!req.user) throw AppError.unauthorized('고객정보를 조회할 수 없습니다.');
+
+  const customerNo = req.user.no;
+  // 고객 상세정보 조회 + 고객 보유 쿠폰 조회
+  const customer = await myService.getMyInfo(customerNo);
+  const couponCount = await myService.getCustomerCouponCount(customerNo);
+  const totalAmount = await myService.getOrderAccumulatedAmount(customerNo);
+  res.status(200).json({ customer, couponCount, totalAmount });
 };
 
 /** 내 정보 수정 */
