195abb01f6f8e419425b9d7f2fc5abcc2b429b23
MARUNGs <mi1764@naver.com>
2025-09-02 13:15:05 +0900

    feat(server) : [비회원] 1년 이내 주문 건 조회 #270

--- apps/server/src/services/order.service.ts
+++ apps/server/src/services/order.service.ts
@@ -313,11 +313,19 @@ export const getGuestOrders = async ({
   ordererMobile: string;
   ordererEmail: string;
 }) => {
+  const now = new Date();
+  const oneYearAgo = new Date();
+  oneYearAgo.setFullYear(now.getFullYear() - 1);
+
   const orders = await prisma.order.findMany({
     where: {
       ordererName,
       ordererMobile,
       ordererEmail,
+      createdAt: {
+        gte: oneYearAgo, // 1년 전 이후부터
+        lte: now, // 현재일자까지
+      },
     },
     include: {
       payment: { select: { isPaid: true } },
